45432675ad4d0e85de62280bf3055e63bc12176a
CodeVINCI <myc.abhi96@gmail.com>
2019-04-07 01:41:58 +0530

    made user model on admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','username','email','is_admin','user_type','first_name','last_name',)
+    list_display_links = ('username',)
+    search_fields = ('username',)
 # Register your models here.
+
+
+admin.site.register(User, UserAdmin)
