3faced7963ea643989af26c7df02bf1695e51079
CodeVINCI <myc.abhi96@gmail.com>
2019-04-06 18:36:29 +0000

    first commit

---
+++ triburg/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect,render
+from django.views.generic import TemplateView
+
+def redirectview(request):
+    if request.user.is_authenticated:
+        return redirect('/home')
+    else:
+        return redirect('/login')
+
+
+
+def homepage(request):
+    return HttpResponse("<h1>This is home page</h1>")
+
+class Loginpage(TemplateView):
+    template = 'loginpage.html'
+    def get(self, request, *wargs, **kwargs):
+        args = {}
+        return render(request,self.template,args)
