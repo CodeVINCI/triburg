587aea455b38edc235b8a738a0fabeaeeb3a11ce
CodeVINCI <myc.abhi96@gmail.com>
2019-04-10 21:30:46 +0530

    create cost sheet page

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import CreateSheet
 
 urlpatterns = [
-
+path('createcostsheet/', CreateSheet.as_view()),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
 # Create your views here.
+
+
+
+class CreateSheet(TemplateView):
+    template_name = 'createsheet.html'
+    def get(self, request, *wargs, **kwargs):
+        args = {}
+        return render(request,self.template_name,args)
